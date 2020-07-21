ccfc3467e5e072e7dc8c0e565ac19d4958512614
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-21 15:55:13 +0000

    Just add one write instead of two in the addres function

--- Function_Sin_Flags_Chapter3.c
+++ Function_Sin_Flags_Chapter3.c
@@ -14,8 +14,7 @@ int Print_Address(va_list Lista)
 	{
 		int Bits;
 
-		write(1, "0", 1);
-		write(1, "x", 1);
+		write(1, "0x", 2);
 		Bits = Print_Number_Hexadecimal_LowerCase((long int)Pointer, 0);
 		return (Bits + 2);
 
